f927d88ad5e155e3ab4f3cafa83e2ce574a0e80a
Jason Johnston <jasonjoh@users.noreply.github.com>
2018-07-13 13:24:39 -0400

    Copy completed 01 module to 03 as baseline

---
+++ Demos/03-add-aad-auth/graph-tutorial/config/initializers/session_store.rb
@@ -0,0 +1,1 @@
+Rails.application.config.session_store :active_record_store, key: '_graph_app_session'
---
+++ Demos/03-add-aad-auth/graph-tutorial/config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  root 'home#index'
+
+  # Add future routes here
+end
