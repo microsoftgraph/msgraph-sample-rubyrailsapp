513a3545d11d6ba50bbf7ed5ba41631652a17c07
Jason Johnston <jasonjoh@users.noreply.github.com>
2018-07-13 14:30:59 -0400

    Add module 3

---
+++ Demos/03-add-aad-auth/graph-tutorial/config/initializers/omniauth_graph.rb
@@ -0,0 +1,8 @@
+require 'microsoft_graph_auth'
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :microsoft_graph_auth,
+           ENV['AZURE_APP_ID'],
+           ENV['AZURE_APP_SECRET'],
+           scope: ENV['AZURE_SCOPES']
+end
--- Demos/03-add-aad-auth/graph-tutorial/config/routes.rb
+++ Demos/03-add-aad-auth/graph-tutorial/config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   root 'home#index'
 
   # Add future routes here
+  get 'auth/signin'
+  get 'auth/signout'
+
+  # Add route for OmniAuth callback
+  match '/auth/:provider/callback', to: 'auth#callback', via: [:get, :post]
 end
