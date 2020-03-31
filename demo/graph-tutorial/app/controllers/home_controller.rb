46417536f52aa7efdd9d46c0fe4ca6b61b2793b3
Jason Johnston <jasonjoh@users.noreply.github.com>
2020-03-31 16:50:07 -0400

    Completed add AAD auth step

--- demo/graph-tutorial/app/controllers/home_controller.rb
+++ demo/graph-tutorial/app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+# HomeController
 class HomeController < ApplicationController
-  def index
-  end
+  def index; end
 end
--- demo/graph-tutorial/config/environment.rb
+++ demo/graph-tutorial/config/environment.rb
@@ -1,5 +1,11 @@
 # Load the Rails application.
 require_relative 'application'
 
+# <LoadOAuthSettingsSnippet>
+# Load OAuth settings
+oauth_environment_variables = File.join(Rails.root, 'config', 'oauth_environment_variables.rb')
+load(oauth_environment_variables) if File.exist?(oauth_environment_variables)
+# </LoadOAuthSettingsSnippet>
+
 # Initialize the Rails application.
 Rails.application.initialize!
