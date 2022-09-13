967a9f5407a3a9dda028367125740d2b8284afcb
dodaromike <v-dodaromike@microsoft.com>
2022-09-13 10:26:34 -0700

    Delete files not needed

--- demo/graph-tutorial/config/initializers/session_store.rb
+++
@@ -1,6 +0,0 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
-
-# <SessionStoreSnippet>
-Rails.application.config.session_store :active_record_store, :key => '_graph_app_session'
-# </SessionStoreSnippet>
