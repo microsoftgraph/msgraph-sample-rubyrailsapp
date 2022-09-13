0e8326bf26773cfd9f0a243c998780f564517fe2
dodaromike <v-dodaromike@microsoft.com>
2022-09-13 14:40:34 -0700

    Revert to earlier commit

---
+++ graph-tutorial/config/initializers/session_store.rb
@@ -0,0 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+# <SessionStoreSnippet>
+Rails.application.config.session_store :active_record_store, :key => '_graph_app_session'
+# </SessionStoreSnippet>
---
+++ graph-tutorial/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  root 'home#index'
+
+  # Add future routes here
+
+end
